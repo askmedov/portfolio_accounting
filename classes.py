8eeef0f7001053401cf60778e6ed6446e1b19282
askmedov <askmedov@gmail.com>
2023-06-10 15:44:55 +0600

    adding checks

--- classes.py
+++ classes.py
@@ -3,10 +3,11 @@ import pandas as pd
 class Asset():
     
     def load_price_history(self, prices: pd.Series):
-        assert isinstance(prices.index, pd.DatetimeIndex)
+        assert isinstance(prices, pd.Series), "Must provide pd.Series"
+        assert isinstance(prices.index, pd.DatetimeIndex), "pd.Series index must be pd.DateTimeIndex"
         self.prices = prices
 
     def update_price(self, prices: pd.Series):
-        assert isinstance(prices.index, pd.DatetimeIndex)
+        assert isinstance(prices, pd.Series), "Must provide pd.Series"
+        assert isinstance(prices.index, pd.DatetimeIndex), "pd.Series index must be pd.DateTimeIndex"
         self.prices = prices.combine_first(self.prices)
-
