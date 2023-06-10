f548eebe254f9fd10630c0a0db4840d1744d8a41
askmedov <askmedov@gmail.com>
2023-06-10 17:40:59 +0600

    adding Debt, updating tests

--- classes.py
+++ classes.py
@@ -1,13 +1,61 @@
+from typing import Union
+
+import numpy as np
 import pandas as pd
+
+
+DT_SERIES_ERROR = "pd.Series index must be pd.DateTimeIndex"
+
 
 class Asset():
     
     def load_price_history(self, prices: pd.Series):
-        assert isinstance(prices, pd.Series), "Must provide pd.Series"
-        assert isinstance(prices.index, pd.DatetimeIndex), "pd.Series index must be pd.DateTimeIndex"
+        assert isinstance(prices.index, pd.DatetimeIndex), DT_SERIES_ERROR
         self.prices = prices
 
     def update_price(self, prices: pd.Series):
-        assert isinstance(prices, pd.Series), "Must provide pd.Series"
-        assert isinstance(prices.index, pd.DatetimeIndex), "pd.Series index must be pd.DateTimeIndex"
+        assert isinstance(prices.index, pd.DatetimeIndex), DT_SERIES_ERROR
         self.prices = prices.combine_first(self.prices)
+
+        
+class Debt(Asset):
+    
+    def __init__(self, 
+                 inception: Union[pd.Timestamp, str] = 'today',
+                 maturity: Union[pd.Timestamp, str] = None,
+                 rate: Union[float, pd.Series] = None,
+                 face_value: Union[int, float] = 1,
+                 pmt_schedule: Union[pd.Series, pd.DataFrame] = None):
+        
+        if isinstance(inception, str):
+            inception = pd.to_datetime(inception)
+        self.inception = inception
+        
+        if isinstance(maturity, str):
+            maturity = pd.to_datetime(maturity)
+        
+        if maturity is not None:
+            assert maturity >= self.inception, "Maturity date is before inception date!"
+        self.maturity = maturity    
+        
+        if isinstance(rate, float) and maturity is not None:
+            dates = pd.date_range(start=self.inception, end=self.maturity, freq='D')
+            rates = np.ones(dates.shape[0]) * rate
+            self.rate = pd.Series(data=rates, index=dates)
+        elif isinstance(rate, pd.Series):
+            assert isinstance(rate.index, pd.DatetimeIndex), DT_SERIES_ERROR
+            assert rate.index.min() >= self.inception
+            assert rate.index.max() <= self.maturity
+            self.rate = rate
+            
+        self.face_value = face_value
+        
+        if type(pmt_schedule) in (pd.Series, pd.DataFrame):
+            assert isinstance(pmt_schedule.index, pd.DatetimeIndex), DT_SERIES_ERROR
+            assert pmt_schedule.index.min() >= self.inception
+            assert pmt_schedule.index.max() <= self.maturity
+        if isinstance(pmt_schedule, pd.DataFrame):
+            err_msg = "columns of payment schedule must be ['interest', 'principal']"
+            assert all(pmt_schedule.columns == ['interest', 'principal']), err_msg
+        self.pmt_schedule = pmt_schedule
+        
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,17 +1,16 @@
 import pytest
+
 import pandas as pd
 import numpy as np
-from ..classes import Asset
+from pandas.testing import assert_series_equal, assert_frame_equal
+
+from ..classes import Asset, Debt
 
 
 def test_Asset():
     asset = Asset()
     with pytest.raises(AssertionError):
-        asset.load_price_history(1)
-    with pytest.raises(AssertionError):
         asset.load_price_history(pd.Series(1))
-    with pytest.raises(AssertionError):
-        asset.update_price(1)
     with pytest.raises(AssertionError):
         asset.update_price(pd.Series(1))
     
@@ -23,7 +22,7 @@ def test_Asset():
     old_prices = pd.Series(data=old_prices, index=dates)
 
     asset.load_price_history(old_prices)
-    assert all(old_prices == asset.prices)
+    assert_series_equal(asset.prices, old_prices)
     
     start_date = pd.Timestamp('2023-06-25')
     end_date = pd.Timestamp('2023-07-05')
@@ -35,6 +34,145 @@ def test_Asset():
     asset.update_price(new_prices)
     old_dates = list(set(old_prices.index) - set(new_prices.index))
     
-    assert all(asset.prices.loc[old_dates] == old_prices[old_dates])
-    assert all(asset.prices.loc[new_prices.index] == new_prices)
+    assert_series_equal(asset.prices.loc[old_dates], old_prices[old_dates])
+    assert_series_equal(asset.prices.loc[new_prices.index], new_prices)
     
+
+def test_Debt():
+    
+    inception_date = '2022-01-01'
+    debt = Debt(inception=inception_date)
+    assert debt.inception == pd.Timestamp(inception_date)
+    
+    with pytest.raises(AssertionError):
+        debt = Debt(inception='2022-01-01', maturity='2021-01-01')
+        
+    maturity_date = '2022-01-05'
+    debt = Debt(inception=inception_date, maturity=maturity_date)
+    assert debt.maturity == pd.Timestamp(maturity_date)
+    
+    rate = 0.05
+    dates = [
+        '2022-01-01',
+        '2022-01-02',
+        '2022-01-03',
+        '2022-01-04',
+        '2022-01-05'
+    ]
+    dates = pd.to_datetime(dates)
+    rates = [rate, rate, rate, rate, rate]
+    expected_rate_schedule = pd.Series(data=rates, index=dates)
+    debt = Debt(rate=rate, maturity=maturity_date, inception=inception_date)
+#     breakpoint()
+    assert_series_equal(debt.rate, expected_rate_schedule, check_freq=False)
+    
+    rate1 = 0.04
+    rate2 = 0.06
+    rates = [rate1, rate1, rate2, rate2, rate2]
+    input_rate_schedule = pd.Series(data=rates)
+    with pytest.raises(AssertionError):
+        debt = Debt(rate=input_rate_schedule, maturity=maturity_date, inception=inception_date)
+    
+    rate = 0.05
+    dates = [
+        '2021-01-01',  #older than inception
+        '2022-01-02',
+        '2022-01-03',
+        '2022-01-04',
+        '2022-01-05'
+    ]
+    dates = pd.to_datetime(dates)
+    rates = [rate, rate, rate, rate, rate]
+    with pytest.raises(AssertionError):
+        debt = Debt(rate=input_rate_schedule, maturity=maturity_date, inception=inception_date)
+    
+    rate = 0.05
+    dates = [
+        '2022-01-01',
+        '2022-01-02',
+        '2022-01-03',
+        '2022-01-04',
+        '2023-01-05'  #later than maturity
+    ]
+    dates = pd.to_datetime(dates)
+    rates = [rate, rate, rate, rate, rate]
+    with pytest.raises(AssertionError):
+        debt = Debt(rate=input_rate_schedule, maturity=maturity_date, inception=inception_date)
+    
+    
+    dates = [
+        '2022-01-01',
+        '2022-01-02',
+        '2022-01-03',
+        '2022-01-04',
+        '2022-01-05'
+    ]
+    dates = pd.to_datetime(dates)
+    input_rate_schedule = pd.Series(data=rates, index=dates)
+    assert_series_equal(debt.rate, input_rate_schedule, check_freq=False)
+    
+    input_pmt_schedule = pd.Series({
+        '2022-01-02': 0.04,
+        '2022-01-05': 1.04
+    })
+    with pytest.raises(AssertionError):
+        debt = Debt(pmt_schedule=input_pmt_schedule, maturity=maturity_date, inception=inception_date)
+        
+    input_pmt_schedule = pd.Series({
+        '2021-01-02': 0.04,  # older than inception
+        '2022-01-05': 1.04
+    })
+    input_pmt_schedule.index = pd.to_datetime(input_pmt_schedule.index)
+    with pytest.raises(AssertionError):
+        debt = Debt(pmt_schedule=input_pmt_schedule, maturity=maturity_date, inception=inception_date)
+        
+    input_pmt_schedule = pd.Series({
+        '2022-01-02': 0.04,
+        '2023-01-05': 1.04  # later than maturity
+    })
+    input_pmt_schedule.index = pd.to_datetime(input_pmt_schedule.index)
+    with pytest.raises(AssertionError):
+        debt = Debt(pmt_schedule=input_pmt_schedule, maturity=maturity_date, inception=inception_date)   
+        
+    input_pmt_schedule = pd.DataFrame({
+        '2022-01-02': [0.04, 0.5],
+        '2022-01-05': [0.04, 0.5]
+    }, index=['rate', 'fv']).T  # incorrect column names
+    input_pmt_schedule.index = pd.to_datetime(input_pmt_schedule.index)
+    with pytest.raises(AssertionError):
+        debt = Debt(pmt_schedule=input_pmt_schedule, maturity=maturity_date, inception=inception_date)
+        
+    input_pmt_schedule = pd.DataFrame({
+        '2022-01-02': [0.04, 0.5],
+        '2022-01-05': [0.04, 0.5]
+    }, index=['interest', 'principal']).T  # incorrect column names
+    input_pmt_schedule.index = pd.to_datetime(input_pmt_schedule.index)
+    debt = Debt(pmt_schedule=input_pmt_schedule, maturity=maturity_date, inception=inception_date)
+    assert_frame_equal(debt.pmt_schedule, input_pmt_schedule, check_freq=False)
+     
+        
+    
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
