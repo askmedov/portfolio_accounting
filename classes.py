dcfa67d1ed806ce213cfdb312f307e6093c74b89
askmedov <askmedov@gmail.com>
2023-06-10 14:45:41 +0600

    checking index type

--- classes.py
+++ classes.py
@@ -3,8 +3,10 @@ import pandas as pd
 class Asset():
     
     def load_price_history(self, prices: pd.Series):
+        assert isinstance(prices.index, pd.DatetimeIndex)
         self.prices = prices
 
     def update_price(self, prices: pd.Series):
+        assert isinstance(prices.index, pd.DatetimeIndex)
         self.prices = prices.combine_first(self.prices)
 
